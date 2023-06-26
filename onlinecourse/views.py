e29d2c6337762bc661a29142bea3c287dbfd1d63
UnknownAlpha <talha.awan5@gmail.com>
2023-06-26 17:44:38 -0400

    changes made from theia lab environment

--- onlinecourse/views.py
+++ onlinecourse/views.py
@@ -134,17 +134,28 @@ def show_exam_result(request, course_id, submission_id):
 
     question_results = []
     total_score = 0
+    max_score = 0
 
     for question in course.question_set.all():
-        is_correct = set(question.choice_set.filter(is_correct=True).values_list('id', flat=True)) == set(selected_choice_ids)
-        grade = question.grade if is_correct else 0
-        total_score += grade
+        selected_choices = question.choice_set.filter(id__in=selected_choice_ids)
+        correct_choices = question.choice_set.filter(is_correct=True)
+
+        is_correct = (
+            selected_choices.count() == correct_choices.count()
+            and selected_choices.filter(is_correct=True).count() == correct_choices.count()
+        )
+
+        grade_point = 0
+        if is_correct:
+            grade_point = question.grade_point
+            total_score += grade_point
+        max_score += question.grade_point
 
         question_result = {
             'question': question,
-            'selected_choices': question.choice_set.filter(id__in=selected_choice_ids),
+            'selected_choices': selected_choices,
             'is_correct': is_correct,
-            'grade': grade,
+            'grade_point': grade_point,
         }
         question_results.append(question_result)
 
@@ -152,9 +163,13 @@ def show_exam_result(request, course_id, submission_id):
         'course': course,
         'selected_choice_ids': selected_choice_ids,
         'total_score': total_score,
+        'max_score': max_score,
         'question_results': question_results,
+        'percentage_score': round((total_score / max_score) * 100, 2),
     }
     return render(request, 'onlinecourse/exam_result_bootstrap.html', context)
+
+
 
 # <HINT> Create a submit view to create an exam submission record for a course enrollment,
 # you may implement it based on following logic:
